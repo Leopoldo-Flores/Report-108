3431f982f52539dbbfced1ae7f3a1ec3b397ee04
Leo <83550516+Leopoldo-Flores@users.noreply.github.com>
2021-07-19 00:49:49 -0700

    Add files via upload

---
+++ data.py
@@ -0,0 +1,81 @@
+data = [
+  {
+    "_id": "1283a9s8d71123123",
+    "title": "renald",
+    "desctiprion": "This is a description of the product",
+    "image": "renald.jpg",
+    "price": 1222.33,
+    "category": "Pet",
+    "discount": 5,
+  },
+
+  {
+    "_id": "1283a9s8d71123124",
+    "title": "john",
+    "desctiprion": "This is a description of the product",
+    "image": "john.jpg",
+    "price": 834.63,
+    "category": "Pet",
+    "discount": 5,
+  },
+
+  {
+    "_id": "1283a9s8d71123125",
+    "title": "armon",
+    "desctiprion": "This is a description of the product",
+    "image": "armon.jpg",
+    "price": 3456.38,
+    "category": "Pet",
+    "discount": 5,
+  },
+
+  {
+    "_id": "1283a9s8d71123126",
+    "title": "farmel",
+    "desctiprion": "This is a description of the product",
+    "image": "farmel.jpg",
+    "price": 837.29,
+    "category": "Pet",
+    "discount": 5,
+  },
+
+  {
+    "_id": "1283a9s8d71123127",
+    "title": "larm",
+    "desctiprion": "This is a description of the product",
+    "image": "larm.jpg",
+    "price": 2258.1,
+    "category": "Pet",
+    "discount": 5,
+  },
+
+  {
+    "_id": "1283a9s8d71123128",
+    "title": "lenard",
+    "desctiprion": "This is a description of the product",
+    "image": "lenard.jpg",
+    "price": 1785.38,
+    "category": "Pet",
+    "discount": 5,
+  },
+
+  {
+    "_id": "1283a9s8d71123129",
+    "title": "perma",
+    "desctiprion": "This is a description of the product",
+    "image": "perma.jpg",
+    "price": 1506.54,
+    "category": "Pet",
+    "discount": 5,
+  },
+
+  {
+    "_id": "1283a9s8d71123121",
+    "title": "benard",
+    "desctiprion": "This is a description of the product",
+    "image": "benard.jpg",
+    "price": 2278.29,
+    "category": "Pet",
+    "discount": 5,
+  },
+]
